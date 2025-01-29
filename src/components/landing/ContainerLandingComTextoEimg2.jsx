59aad23107637cc9d63ef6d7df0d58993de6082e
Matheus De O Zacanini <zacanini2@gmail.com>
2025-01-29 18:35:03 -0300

    💻 [WIP] feat(page): implementando layout landing page (0.0.1 version)

---
+++ src/components/landing/ContainerLandingComTextoEimg2.jsx
@@ -0,0 +1,15 @@
+export default function ContainerDois({ imgSrc, title, description }) {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px', boxSizing: 'border-box' }}>
+            <div style={{ display: 'flex', backgroundColor: '#daa520', padding: '20px', boxSizing: 'border-box', alignItems: 'center', maxWidth: 'calc(100% - 100px)', margin: '0 50px', marginLeft:'90px' }}>
+                <div style={{ backgroundColor: '#000', borderRadius: '5px', color: '#fff', padding: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <h1 style={{ textTransform: 'uppercase' }}>{title}</h1>
+                    <p style={{ fontSize: '22px', textTransform: 'uppercase' }} dangerouslySetInnerHTML={{ __html: description }}></p>
+                </div>
+                <div style={{ flex: '0 0 auto', padding: '20px' }}>
+                    <img src={imgSrc} alt="Imagem" style={{ width: '300px', height: 'auto' }} />
+                </div>
+            </div>
+        </div>
+    );
+}
